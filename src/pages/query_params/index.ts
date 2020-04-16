2924d195bd35ceb3eb902b850b5c43501d84960a
li_kou <contact@awesome-linus.com>
2020-04-17 03:01:48 +0900

    add query

--- src/pages/query_params/index.ts
+++ src/pages/query_params/index.ts
@@ -2,7 +2,15 @@ import Vue from 'vue';
 import { Context } from '@nuxt/types';
 
 export default Vue.extend({
-  async fetch({ query }: Context) {
-    await console.log(query);
+  watchQuery: true,
+  asyncData({ query }: Context) {
+    const selectedQuery = query.query || 'nothing';
+    return { selectedQuery };
+  },
+  methods: {
+    async selectQuery(event: Event): Promise<void> {
+      const value = (<HTMLInputElement>event.target).value;
+      await this.$router.push(`/query_params?query=${value}`);
+    }
   }
 });
--- src/pages/query_params/index.vue
+++ src/pages/query_params/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    aaa
+  <div :class="$style.container">
+    <div :class="$style.selectBox">
+      <select required @change="selectQuery">
+        <option value="" hidden>
+          選択してください
+        </option>
+        <option value="query1">
+          query1
+        </option>
+        <option value="query2">
+          query2
+        </option>
+        <option value="query3">
+          query3
+        </option>
+        <option value="query4">
+          query4
+        </option>
+      </select>
+    </div>
+    <div :class="$style.selectedBox">
+      <p>selectedQuery: {{ selectedQuery }}</p>
+    </div>
   </div>
 </template>
 
