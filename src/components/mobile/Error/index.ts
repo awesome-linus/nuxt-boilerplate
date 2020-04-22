25f487c6fba3279ed93786447adebd8a22d16701
li_kou <contact@awesome-linus.com>
2020-04-23 02:50:21 +0900

    update

--- src/components/mobile/Error/index.ts
+++ src/components/mobile/Error/index.ts
@@ -1,13 +1,11 @@
 import Vue from 'vue';
 import Error404 from '~/components/mobile/Error/Error404';
 import Error500 from '~/components/mobile/Error/Error500';
-import Error503 from '~/components/mobile/Error/Error503';
 
 export default Vue.extend({
   components: {
     Error404,
-    Error500,
-    Error503
+    Error500
   },
   props: {
     error: {
