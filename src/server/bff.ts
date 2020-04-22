9a43b78cf8ec1a819eb836e2313b477772ec06e6
li_kou <contact@awesome-linus.com>
2020-04-23 03:07:42 +0900

    update

--- src/server/bff.ts
+++ src/server/bff.ts
@@ -4,21 +4,20 @@ import errorHandling from './router/api/errorHandling';
 import healthcheck from './router/api/healthcheck';
 
 const router = Router();
+const bff = express();
+
+// Maintenance Mode
+bff.use((req, res, next) => {
+  if (req.url === '/api/healthcheck') {
+    return next();
+  }
+  return res.redirect('/errors/503.html');
+});
+
+bff.use('/api', router);
 
 router.use(bffSamples);
 router.use(errorHandling);
 router.use(healthcheck);
-
-const bff = express();
-
-// Maintenance Mode
-// bff.use((req, res, next) => {
-//   if (req.url === '/healthcheck') {
-//     return next();
-//   }
-//   return res.redirect('/errors/503.html');
-// });
-
-bff.use('/api', router);
 
 module.exports = bff;
