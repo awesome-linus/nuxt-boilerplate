bdcb91123c1453cb6b96f183d0ad3ee8bbe4733d
li_kou <contact@awesome-linus.com>
2020-04-25 20:01:19 +0900

    update

--- server/config/index.ts
+++ server/config/index.ts
@@ -1,8 +1,9 @@
 // This is for Server Side usage.
 // confidential information such as secret keys.
+// Please Set Environment Variable With Direnv(local) or AWS Secret Manager(production).
 
 const env = {
-  anySecretKey: 'anySecretKeyValue'
+  anySecretKey: process.env.ANY_SECRET_KEY
 };
 
 export default env;
