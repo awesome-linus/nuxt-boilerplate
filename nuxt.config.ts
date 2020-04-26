213b4f7b2347fe2878e4ddd454751f384ceee30c
li_kou <contact@awesome-linus.com>
2020-04-26 12:23:12 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import env from './config';
 
 const nuxtConfig: Configuration = {
   mode: 'universal',
+  server: {
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || '0.0.0.0'
+  },
   srcDir: 'src',
   env,
   serverMiddleware: ['~~/server/bff.ts'],
